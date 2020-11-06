cfe9eb91e90d2d357059eefb0b6f46cb71fbd896
unknown <pelumioni25@gmail.com>
2020-11-05 18:08:58 -0800

    m

--- src/App.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>
-    <router-view name="nav" />
-    <router-view/>
-    <router-view name="footer" />
-  </div>
-</template>
--- src/assets/js/bootstrap.js
+++
@@ -1,9 +0,0 @@
-import 'jquery';
-import 'popper.js';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.css';
-import '../css/style.css';
-export default {
-  install(Vue) {
-  }, 
-};
